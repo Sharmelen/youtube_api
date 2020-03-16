e7e5808d3c376c2e962ea634155335f6214fd696
Sharmelen <33871504+Sharmelen@users.noreply.github.com>
2020-03-17 05:50:43 +0800

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -1,11 +1,13 @@
 from youtube_api import YouTubeDataAPI
 import pafy
+from bs4 import BeautifulSoup
+import requests
+import re
 
-api_key = 'USE YOUR OWN API KEY'
+api_key = 'AIzaSyASfy22lmaxSt2-MbAk9nMZql3wTtLFSZ0'
 yt = YouTubeDataAPI(api_key)
 
 searches = yt.search(q='ryan', max_results=2)
-
 
 data = searches[1]
 print("video_id: ",data['video_id'])
@@ -19,6 +21,8 @@ print("Video Category : ",data['video_category'])
 print("Video Thumbnail : ",data['video_thumbnail'])
 print("Date Collected : ",data['collection_date'])
 
+url = 'https://www.youtube.com/watch?v={}'.format(video_id)
+
 video = pafy.new('https://www.youtube.com/watch?v={}'.format(video_id))
 
 print('https://www.youtube.com/watch?v={}'.format(video_id))
@@ -27,3 +31,19 @@ print("video length : ",video.length)
 print("video likes : ", video.likes)
 print("video dislikes : ",video.dislikes)
 print("video views : ",video.viewcount)
+
+
+page = requests.get(url)
+soup = BeautifulSoup(page.content, 'html.parser')
+#print(soup.prettify())
+
+view_count = soup.find_all('a',class_="content-link spf-link yt-uix-sessionlink spf-link")
+
+list = str(view_count).split()
+
+
+related_link = [i for i in list if i.startswith('href')]
+
+for x in range (len(related_link)):
+
+    print('https://www.youtube.com{}'.format(related_link[x].strip('href=""')))
